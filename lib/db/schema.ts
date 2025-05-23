561e370a8af58921d6584293e01dff6c8faa4d4d
théau Yapi <theau.yapi@ynov.com>
2025-05-23 14:59:38 +0200

    schema

---
+++ lib/db/schema.ts
@@ -0,0 +1,32 @@
+import {pgTable, text,uuid,integer,boolean, timestamp} from "drizzle-orm/pg-core"
+import {relations} from "drizzle-orm"
+
+export const files = pgTable("files", {
+    id: uuid("id").defaultRandom().primaryKey(),
+
+    //basic folder informùation
+    name: text("name").notNull(),
+    path: text("path").notNull(), // /documents/project/resume.pdf
+    size: integer("size").notNull(),
+    type: text("type").notNull(), //"folder" or "file"
+
+    //storage information
+    fileUrl: text("file_url").notNull(), // url to access the file
+    thumbnailUrl: text("thumbnail_url").notNull(), // url to access the thumbnail
+
+    //ownership information
+    userId: text("user_id").notNull(), // user who uploaded the file
+    parentId: uuid("parent_id"), // parent folder id (null for root items)
+
+    //file/folder flags
+    isFolder: boolean("is_folder").default(false).notNull(), // true if the item is a folder
+    isStarred: boolean("is_starred").default(false).notNull(), // true if the item is starred
+    isTrash: boolean("is_trash").default(false).notNull(), // true if the item is in trash
+
+    //Timestamps
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+})
+    
+
+    
